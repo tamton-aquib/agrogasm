87e4e0b6a47cea264779af3b34c3a0b9198da735
Mhdafh <mhdafh@tuta.io>
2022-06-26 13:28:33 +0530

    final css and stuff

--- src/components/NewPost.tsx
+++ src/components/NewPost.tsx
@@ -1,14 +1,18 @@
+import './NewPost.css';
 const NewPost = () => {
 
     return (
         <div className="container">
+            <h3>Add new post</h3>
             <form>
-                <input name="title" />
-                <input name="description" />
+                <input placeholder="Title" name="title" className="title" />
+                <input placeholder="Description" className="description" />
 
                 <button>Submit</button>
             </form>
+            
         </div>
+
     )
 }
 
